3f2d54342905679c1155246d371e286573eecf22
IagoSouza <iago.csouza15@gmail.com>
2024-03-25 15:20:37 -0300

    config inicial

---
+++ api/src/test/java/com/fatec/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
